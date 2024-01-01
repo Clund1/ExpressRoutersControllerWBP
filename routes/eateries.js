642c4b95daaf433a5994d2983500385fd92dd63b
CHL <Clundquist1@gmail.com>
2024-01-01 17:04:17 -0500

    MVP

--- routes/eateries.js
+++ routes/eateries.js
@@ -1,20 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('eateries/list');
-});
+const eateriesCtrl = require('../controllers/eateries');
 
-router.get('/3226', function(req, res, next) {
-  res.render('eateries/3226');
-});
-
-router.get('/5475', function(req, res, next) {
-  res.render('eateries/5475');
-});
-
-router.get('/9567', function(req, res, next) {
-  res.render('eateries/9567');
-});
+router.get('/', eateriesCtrl.index);
+router.get('/:id', eateriesCtrl.show);
 
 module.exports = router;
